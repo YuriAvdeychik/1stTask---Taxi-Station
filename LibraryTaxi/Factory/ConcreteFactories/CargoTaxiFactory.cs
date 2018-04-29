23bddd5041f9ff3872578e89b2dbf1f11d3f271f
Yuri Avdeychik <yuri.avdeychik@gmail.com>
2018-04-29 17:01:08 +0300

    2 Create ierarchy

--- LibraryTaxi/Factory/ConcreteFactories/CargoTaxiFactory.cs
+++ LibraryTaxi/Factory/ConcreteFactories/CargoTaxiFactory.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryTaxi
+namespace LibraryTaxi.Factory.ConcreteFactories
 {
-    public class Class1
+    class CargoTaxiFactory
     {
     }
 }
